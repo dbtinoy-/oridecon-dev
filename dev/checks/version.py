0aa16a3a50b6f704ae28123067b1b6dc89a4c292
dbtinoy- <dbtinoy@gmail.com>
2026-08-26 06:36:29 +0800

    🔧 chore(dev,contracts): fix version.py ROOT import; bump contracts 0.1.5005

--- dev/checks/version.py
+++ dev/checks/version.py
@@ -46,6 +46,7 @@ import urllib.error
 import urllib.request
 
 
+from dev.core.bootstrap import REPO_ROOT as ROOT
 from dev.core.package_inventory import discover_package_paths
 
 PYPI_URL = "https://pypi.org/pypi/{name}/json"
